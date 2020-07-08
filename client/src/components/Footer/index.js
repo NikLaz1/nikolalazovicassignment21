cee1c2fd0ba10fe6cff43f3750940461cc9269b8
Nikola Lazovic <nikolalazovic@tissaro.com>
2020-07-08 11:53:08 -0400

    Initial Check In

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const Footer = () => (
+	<footer className = "page-footer">
+		<div className = "container-fluid text-center">
+			<p>Powered by Google Books API - Nikola Lazovic</p>
+		</div>	
+	</footer>
+);
+
+export default Footer;
